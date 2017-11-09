e2964d2813a716195059c515b2b784d7a6c8c9bb
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-11-08 23:54:54 -0200

    Making the request on the api

--- src/Components/Posts/index.js
+++ src/Components/Posts/index.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { postsFetchData } from '../../actions/posts'
 
-const Posts = () => {
-  return (
-    <h1>Posts page</h1>
-  )
+class Posts extends Component {
+  componentDidMount() {
+    this.props.fetchData()
+  }
+  render() {
+    if (this.props.hasErrored) {
+      return <h1>Sorry but there was an error while fetch</h1>
+    }
+    if (this.props.isLoading) {
+      return <h1>Loading ...</h1>
+    }
+    return (
+      <ul>
+        {this.props.posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    )
+  }
 }
 
-export default Posts
+const mapStateToProps = (state) => {
+  return {
+      posts: state.posts,
+      hasErrored: state.postsHasErrored,
+      isLoading: state.postsIsLoading
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      fetchData: () => dispatch(postsFetchData())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Posts)
--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,3 +1,5 @@
+import { get } from '../utils/api'
+
 export function postsIsLoading(bool) {
   return {
     type: 'POSTS_IS_LOADING',
@@ -16,5 +18,19 @@ export function postsFetchDataSuccess(posts) {
   return {
     type: 'POSTS_FETCH_DATA_SUCCESS',
     posts
+  }
+}
+
+export function postsFetchData() {
+  const url = 'http://localhost:3001/posts'
+  return (dispatch) => {
+    dispatch(postsIsLoading(true))
+    get(url)
+      .then(res => {
+        dispatch(postsIsLoading(false))
+        return res.data
+      })
+      .then(posts => dispatch(postsFetchDataSuccess(posts)))
+      .catch(() => dispatch(postsHasErrored(true)))
   }
 }
